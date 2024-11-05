30ed3a65ff7008753d1385c0c92d719493b71dba
luongbtph52665 <luongbtph52665@gmail.com>
2024-11-06 00:28:18 +0700

    branch-Bui-Luong

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@ require_once '../config/connect.php'; // Connect to database
 require_once 'controllers/DashboardController.php';
 require_once 'controllers/BlogController.php';
 require_once 'controllers/CategoryController.php';
-require_once 'controllers/BannerContraoller.php';
+require_once 'controllers/BannerController.php';
 
 // Require toàn bộ file Models
 require_once 'models/BlogModel.php';
