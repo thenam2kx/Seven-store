43bbc73aae4761892ff219318273043e21b36ea6
thenam2kx <thenam2kx@gmail.com>
2024-11-06 20:54:11 +0700

    fix new key when update database

--- admin/models/CategoryModel.php
+++ admin/models/CategoryModel.php
@@ -8,7 +8,7 @@ class CategoryModel {
 
   public function getAll($limit = 10, $page = 1) {
     $offset = ($page - 1) * $limit;
-    $sql = "SELECT * FROM `danh_mucs` ORDER BY danh_muc_id DESC LIMIT $offset, $limit";
+    $sql = "SELECT * FROM `danh_mucs` ORDER BY id DESC LIMIT $offset, $limit";
     return $this->db->query($sql);
   }
   public function getTotalPage() {
@@ -17,23 +17,23 @@ class CategoryModel {
     return $result[0]['total'];
 }
 
-  public function getOne($danh_muc_id) {
-    $sql = "SELECT * FROM `danh_mucs` WHERE danh_muc_id=?";
-    return $this->db->queryOne($sql, $danh_muc_id);
+  public function getOne($id) {
+    $sql = "SELECT * FROM `danh_mucs` WHERE id=?";
+    return $this->db->queryOne($sql, $id);
   }
 
-  public function create($ten_danh_muc, $mo_ta, $trang_thai = 1) {
-    $sql = "INSERT INTO danh_mucs (ten_danh_muc, trang_thai, mo_ta) VALUES (?, ?, ?)";
-    return $this->db->execute($sql, $ten_danh_muc, $trang_thai, $mo_ta);
+  public function create($ten_danh_muc, $trang_thai = 1) {
+    $sql = "INSERT INTO danh_mucs (ten_danh_muc, trang_thai) VALUES (?, ?)";
+    return $this->db->execute($sql, $ten_danh_muc, $trang_thai);
   }
 
-  public function edit($ten_danh_muc, $trang_thai, $mo_ta, $danh_muc_id) {
-    $sql = "UPDATE danh_mucs SET ten_danh_muc=?, trang_thai=?, mo_ta=?, cap_nhat=CURRENT_TIMESTAMP WHERE danh_muc_id=?";
-    return $this->db->execute($sql, $ten_danh_muc, $trang_thai, $mo_ta, $danh_muc_id);
+  public function edit($ten_danh_muc, $trang_thai, $id) {
+    $sql = "UPDATE danh_mucs SET ten_danh_muc=?, trang_thai=? WHERE id=?";
+    return $this->db->execute($sql, $ten_danh_muc, $trang_thai, $id);
   }
 
-  public function delete($danh_muc_id) {
-    $sql = "DELETE FROM `danh_mucs` WHERE danh_muc_id=?";
-    return $this->db->execute($sql, $danh_muc_id);
+  public function delete($id) {
+    $sql = "DELETE FROM `danh_mucs` WHERE id=?";
+    return $this->db->execute($sql, $id);
   }
 }
